bc2f55c4dd07924a9ae9b6e7efee4093c4da1681
Tootbrute <tootbrute@tutanota.com>
2024-11-12 21:20:14 +0800

    Update NixOS config.

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.elias = import ./nixos-config/home.nix;
+            home-manager.users.elias = import ./home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
