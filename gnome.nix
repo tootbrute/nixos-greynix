b4f61417301c15be3e9c9984048c7d874e0e18d7
Tootbrute <tootbrute@tutanota.com>
2024-11-14 11:21:32 +0800

    Update NixOS config.

--- gnome.nix
+++ gnome.nix
@@ -12,7 +12,8 @@
     cheese      # photo booth
     eog         # image viewer
     epiphany    # web browser
-    #gedit       # text editor
+    evince      # pdfs
+    #gedit      # text editor
     simple-scan # document scanner
     totem       # video player
     yelp        # help viewer
