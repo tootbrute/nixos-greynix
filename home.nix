b1d9bf3e4c02793a7677ddf546375fe981df2eef
Tootbrute <tootbrute@tutanota.com>
2024-11-20 15:51:31 +0800

    Update NixOS config.

--- home.nix
+++ home.nix
@@ -23,6 +23,7 @@
   home.packages = with pkgs; [
     # here is some command line tools I use frequently
     # feel free to add your own or remove some of them
+    hello
 
     cinnamon.xreader #pdf viewer
 
