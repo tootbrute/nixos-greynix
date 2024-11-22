baa9f81464146493b7c2f143bbfdec2e39916e11
Tootbrute <tootbrute@tutanota.com>
2024-11-22 15:25:35 +0800

    Update NixOS config.

--- pull.sh
+++ pull.sh
@@ -1,10 +1,6 @@
 # pull new stuff down
+echo "pulling nixos config from git repo"
 git pull origin main
-echo "rebuilding" $(hostname) "from git repo"
-git pull origin main
-
-# build it
-sudo nixos-rebuild switch --flake '.#$(hostname)'
 
 # update nixos
 # sudo nixos-rebuild switch --flake .#HOST
