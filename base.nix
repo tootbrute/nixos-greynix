24a4221fc0f494cad0c385d3cd5f889a43190993
tootbrute <tootbrute@tutanota.com>
2025-01-05 07:58:31 +0800

    Update NixOS config.

--- base.nix
+++ base.nix
@@ -3,9 +3,9 @@
 {
   imports =
     [ # I do imports in flakes
-      ./gnome.nix
-      ./systemd.nix
-      ./fonts.nix
+      ./nixosModules/gnome.nix
+      ./nixosModules/systemd.nix
+      ./nixosModules/fonts.nix
     ];
 
   # Bootloader.
