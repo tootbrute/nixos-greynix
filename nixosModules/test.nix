fc8352ee4c9c757e58804e0ce42184762cf66750
tootbrute <tootbrute@tutanota.com>
2025-01-05 08:47:22 +0800

    Update NixOS config.

--- nixosModules/test.nix
+++ nixosModules/test.nix
@@ -1,7 +1,16 @@
-{ config, pkgs, ... }:
+{ pkgs, lib, config, ... }:
+# enable docker
+# not all systems need this
 
 {
 
+  options = {
 
+  };
+
+  config = {
+    # Enable Docker
+    virtualisation.docker.enable = true;
+  };
 
 }
