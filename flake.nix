f9f2b6f5fe7055c8610b733efa4f3dff72907cb9
tootbrute <tootbrute@tutanota.com>
2025-01-12 21:51:33 +0800

    cleaned up flake

--- flake.nix
+++ flake.nix
@@ -2,10 +2,12 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+      #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      home-manager = {
+          url = "github:nix-community/home-manager";
+          inputs.nixpkgs.follows = "nixpkgs";
+      };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
