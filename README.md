747524493d76461a95218e706865b42c85d2306f
tootbrute <tootbrute@tutanota.com>
2025-01-12 19:22:01 +0800

    first commit

--- README.md
+++ README.md
@@ -1,38 +1,5 @@
 # My NixOS Flake Config
-My NixOS repo with that has the configs for 2 of my NixOS machines.
+My NixOS repo for a single machine.
 
-## NOTE: Archived.
-I don't use this anymore. It got too complicated with multiple machines.
-
-# Structure of NixOS Config
-
-```.
-├── flake.lock
-├── flake.nix
-├── flatpak-install.sh
-├── home.nix
-├── hosts
-│   ├── aspirenix
-│   │   ├── configuration.nix
-│   │   └── hardware-configuration.nix
-│   ├── bluenix
-│   │   ├── configuration.nix
-│   │   └── hardware-configuration.nix
-│   └── greynix
-│       ├── configuration.nix
-│       └── hardware-configuration.nix
-├── nixosModules
-│   ├── base.nix
-│   ├── docker.nix
-│   ├── fonts.nix
-│   ├── gnome.nix
-│   ├── module-docker.nix
-│   ├── module-modules.nix
-│   └── systemd.nix
-├── nix-upgrade.sh
-├── pull.sh
-├── README.md
-├── rebuild.sh
-├── temp-non-declarative-stuff.txt
-└── update.sh
-```
+- Flakes
+- Homemanager
--- flake.nix
+++ flake.nix
@@ -21,54 +21,14 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.elias = import ./home.nix;
+            home-manager.users.elias = import .nixosModules/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
-          } #close home-manager
-        ]; #close modules
-       }; #close greynix
+          } 
+        ]; 
+       }; 
 
-        # bluenix - Blue Acer machine
-        bluenix = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nixosModules/base.nix
-          ./hosts/bluenix/configuration.nix
-          ./nixosModules/docker.nix
-          
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.elias = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          } #close homemanager
-        ]; #close modules
-      }; # close bluenix
-
-        # aspirenix - Grey Acer Aspire machine
-        # low spec machine - need to figure out how to change this config a bit
-        aspirenix = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nixosModules/base.nix
-          ./hosts/aspirenix/configuration.nix
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.elias = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          } #close homemanager
-        ]; #close modules
-      }; # close bluenix
-
-    }; #close nixosconfigurations
-  }; #close outputs
+    }; 
+  }; 
 }
