03cb349971a1dd5ceb091aaa39bda7e38a839336
tootbrute <tootbrute@tutanota.com>
2025-01-16 06:32:20 +0800

    Update NixOS config.

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 # add things to git
+COMMIT_MSG="Update NixOS config."
+echo "commit message?"
+read $COMMIT_MSG
 git add .
-git commit -m "Update NixOS config."
+git commit -m "$COMMIT_MSG"
 git push origin main
