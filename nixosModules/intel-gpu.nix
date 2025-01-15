de8e55a3e301ea6298a5b557d24b9d576d5ddb84
tootbrute <tootbrute@tutanota.com>
2025-01-16 06:26:26 +0800

    Update NixOS config.

--- nixosModules/intel-gpu.nix
+++ nixosModules/intel-gpu.nix
@@ -8,15 +8,8 @@
     extraPackages = with pkgs; [
       intel-media-driver # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
       intel-vaapi-driver # For older processors. LIBVA_DRIVER_NAME=i965, works better for firefox/chromium?
+      vpl-gpu-rt # For Quick Sync Video
     ];
   };
   environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Optionally, set the environment variable
 }
-
-# for Quick Sync Video
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      vpl-gpu-rt # or intel-media-sdk for QSV
-    ];
-  };
