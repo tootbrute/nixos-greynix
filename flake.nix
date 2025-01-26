cf479cb98bacc1e3eded2fddc03b85b4490f6d94
tootbrute <tootbrute@tutanota.com>
2025-01-26 11:09:20 +0800

    removed udiskie

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-      #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
       home-manager = {
           url = "github:nix-community/home-manager";
           inputs.nixpkgs.follows = "nixpkgs";
--- nixosModules/home.nix
+++ nixosModules/home.nix
@@ -120,8 +120,6 @@
 
   };
 
-  services.udiskie.enable = true;
-
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
