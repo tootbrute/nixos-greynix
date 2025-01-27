c51bc454bca305295675f69d8600d0f1eccf1728
tootbrute <tootbrute@tutanota.com>
2025-01-27 16:18:02 +0800

    fixed NVF setup

--- nixosModules/neovim-config.nix
+++ nixosModules/neovim-config.nix
@@ -3,23 +3,25 @@
 {
   # neovim setup with NVF
   # https://github.com/NotAShelf/nvf
-  vim = {
-    theme = {
-      enable = true;
-      name = "gruvbox";
-      style = "dark";
-    };
-    statusLine.lualine.enable = true;
-    telescope.enable = true;
-    autocomplete.nvim-cmp.enable = true;
+  programs.nvf = {
+    vim = {
+      theme = {
+        enable = true;
+        name = "gruvbox";
+        style = "dark";
+      };
+      statusLine.lualine.enable = true;
+      telescope.enable = true;
+      autocomplete.nvim-cmp.enable = true;
     
-    languages = {
-      enableLSP = true;
-      enableTreesitter = true;
+      languages = {
+        enableLSP = true;
+        enableTreesitter = true;
       
-      nix.enable = true;
-      ts.enable = true;
-      rust.enable = true;
+        nix.enable = true;
+        ts.enable = true;
+        rust.enable = true;
+      };
     };
   };
 
